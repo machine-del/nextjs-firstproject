78218b8a9e168b3ab1c2b185e1703414de15dbe7
machine <137556120+machine-del@users.noreply.github.com>
2025-09-29 09:20:00 +0500

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Lusitana } from "next/font/google";
 export const inter = Inter({ subsets: ["latin"], preload: false });
-export const lusitina = Lusitana({ subsets: ['latin'], weight: ["400"], preload: false });
+export const lusitina = Lusitana({ weight: ["400"], subsets: ['latin'], preload: false });
+
 
