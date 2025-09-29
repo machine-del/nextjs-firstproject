b26f20ac57f734f619b7c96c57b4825c163962e0
machine <137556120+machine-del@users.noreply.github.com>
2025-09-29 09:15:47 +0500

    feat: edit value fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter } from "next/font/google";
 import { Lusitana } from "next/font/google";
-export const inter = Inter({ subsets: ["latin"] });
-export const lusitina = Lusitana({ weight: ["400"] });
+export const inter = Inter({ subsets: ["latin"], preload: false });
+export const lusitina = Lusitana({ weight: ["400"], preload: false });
